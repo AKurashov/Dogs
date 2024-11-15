63f485ed971ad0f5900f67c8e44d94fd312fee9b
akurashov <Alkurashov@gmail.com>
2024-11-15 11:50:04 +0400

    Добавлен progress bar

--- Голиков Денис/Dogs.py
+++ Голиков Денис/Dogs.py
@@ -1,5 +1,6 @@
 from  tkinter import *
 from  tkinter import messagebox as mb
+from tkinter import ttk
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
@@ -42,15 +43,22 @@ def show_image():
                 label.image = img
             except Exception as e:
                 mb.showerror("Ошибка", f"Возникла ошибка {e} при загрузке изображения")
+        progress.stop()
 
-
+def progr():
+    progress["value"] = 0
+    progress.start(30)
+    window.after(3000, show_image)
 window = Tk()
 window.title("Картинки с собачками")
 window.geometry("360x420")
 
-label = Label()
+label = ttk.Label()
 label.pack(pady = 10)
 
-button = Button(text = "Загрузить изображение", command=show_image)
+button = ttk.Button(text = "Загрузить изображение", command=progr)
 button.pack(pady=10)
+progress = ttk.Progressbar(mode = "determinate", length=300)
+progress.pack(pady = 10)
+
 window.mainloop()
